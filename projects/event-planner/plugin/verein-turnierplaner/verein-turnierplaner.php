8b78374e05cb598b54d9136c1da7ae2d8a7e43a1
vaan0011 <anton.vasilj@tus-mingolsheim.de>
2026-09-19 14:33:18 +0200

    Event Planner: Version anheben und History-Doppelhook entfernen

--- projects/event-planner/plugin/verein-turnierplaner/verein-turnierplaner.php
+++ projects/event-planner/plugin/verein-turnierplaner/verein-turnierplaner.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: Verein Turnierplaner
  * Description: Turnier-, Event- und Helferplanung für Fußballvereine: Turniere, Spielpläne, Ergebnisse, QR-Code, Sportfest-Ablauf, Bewirtung und Schichtpläne.
- * Version: 3.8.14
+ * Version: 3.8.15
  * Author: TuS 1901 Mingolsheim e.V.
  * Author URI: https://tus-mingolsheim.de
  * Text Domain: verein-turnierplaner
  */
 if (!defined('ABSPATH')) exit;
-define('VTP_VERSION', '3.8.14');
+define('VTP_VERSION', '3.8.15');
 define('VTP_FILE', __FILE__);
 define('VTP_DIR', plugin_dir_path(__FILE__));
 define('VTP_URL', plugin_dir_url(__FILE__));
@@ -30,7 +30,8 @@ add_action('plugins_loaded', function(){
  VTP_Plugin::instance();
  VTP_Public::instance();
  VTP_Dashboard::init();
- VTP_Dashboard_History::init();
+ // Die Historie wird explizit innerhalb von VTP_Dashboard::render() ausgegeben.
+ // Kein zweiter Dashboard-Hook, sonst erscheint der Block doppelt.
  VTP_Event_Create_UI::init();
  VTP_Event_Edit_UI::init();
  VTP_Event_Day_Planner::init();
